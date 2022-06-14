7bf6d077a012e9b0780b300977d8087c8e4c421c
Colloke <github.p5x18@simplelogin.co>
2022-06-14 11:52:29 -0500

    more comments

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
-import pic from "./mountain1-01.jpg"
+import pic from "./mountain1-01.jpg"; //import neccessary files
 
 const container = document.getElementById('root');  //setting up variables for root.render later
 const root = createRoot(container);
 
-const logo = (
+const header = (
     <div>
         <img src={pic} alt="A logo of a mountain" width="100px"/> 
     </div>
 ) // logo top left
 
 root.render(
-    logo,
-)
+    header,
+) //render it!
