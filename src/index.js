c76c5a1997a3dfb363741009c84fb12c2377bead
Colloke <106322207+Colloke@users.noreply.github.com>
2022-06-14 11:25:20 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import { createRoot } from 'react-dom/client';
 import pic from "./mountain1-01.jpg"
 
-const container = document.getElementById('root');
+const container = document.getElementById('root');  //setting up variables for root.render later
 const root = createRoot(container);
 
-const app = (
+const logo = (
     <div>
-        <img src={pic} alt="A logo of a mountain" width="100px"/>
+        <img src={pic} alt="A logo of a mountain" width="100px"/> //logo image top left
     </div>
 )
 
 root.render(
-    app,
+    logo,
 )
