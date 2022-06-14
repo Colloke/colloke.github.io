897b5deb94dc1eb27f09d3bf6a510fed13c8e8ea
Colloke <github.p5x18@simplelogin.co>
2022-06-14 12:09:23 -0500

    turned the variable into a react component

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import pic from "./mountain1-01.jpg"; //import neccessary files
 const container = document.getElementById('root');  //setting up variables for root.render later
 const root = createRoot(container);
 
-const header = (
+function Header() {
+  return(
     <div>
         <img src={pic} alt="A logo of a mountain" width="100px"/> 
     </div>
-) // logo top left
+  )
+} // logo at top left as a react component
 
 root.render(
-    header,
-) //render it!
+    <Header />,
+) //render it! The < /> turns it into a component
