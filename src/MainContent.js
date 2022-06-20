e6ea18918de2b628d7aa91937f03553c08fe68f3
Colloke <github.p5x18@simplelogin.co>
2022-06-19 19:15:12 -0500

    a

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Random_Number_Generator from "./Random_Number_Generator/RandNumGen";
+import MainContentCss from "./index.css";
 
 export function MainContent() {
     return (
